6a83a2d1c03de32d02bdf13b457509d81a267491
Sridhanam <dharsri255@gmail.com>
2025-06-01 13:35:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
-
 import React from "react";
 import './../styles/App.css';
+import Autocomplete from "./AutoComplete"
 
 const App = () => {
+  let fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
   return (
     <div>
         {/* Do not remove the main div */}
+    <Autocomplete  suggestions={fruits}/>
     </div>
   )
 }
 
 export default App
+
